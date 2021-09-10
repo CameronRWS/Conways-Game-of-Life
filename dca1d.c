92bec876b1f60f63064b9480d9a0e3aba9a79d2e
Cameron <CameronRWS@gmail.com>
2021-09-10 16:08:07 -0500

    number 2, all in one file

---
+++ dca1d.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+#include <string.h>
+
+
+void init1DCA(int numCellsToCreate, char *startOfString) {
+    for(int i = 0; i < numCellsToCreate - 1; i++) {
+        startOfString[i] = '0';
+    }
+    startOfString[numCellsToCreate-1] = '\0';
+}
+
+int main(void) {
+    int lengthOfAuto = 32;
+    char dca1d[lengthOfAuto];
+    init1DCA(lengthOfAuto, dca1d);
+    printf("%s\n", dca1d);
+    return 0;
+}
